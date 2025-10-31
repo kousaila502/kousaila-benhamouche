76c306548ecfa7d344917253e3d226fdf743aa69
Kousaila Benhamouche <k.benhamouche@esi-sba.dz>
2025-10-31 21:19:10 +0100

    dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class', // enable class strategy so `document.documentElement.classList.add('dark')` works
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
